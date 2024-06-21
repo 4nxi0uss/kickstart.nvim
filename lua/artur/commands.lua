8b22d6fd6bb5f1bb627c0cf76c7a533da748fc9c
4nxi0uss <artur200212@interia.pl>
2024-06-22 01:58:07 +0200

    fix: fixing format command

--- lua/artur/commands.lua
+++ lua/artur/commands.lua
@@ -1,5 +1,48 @@
-vim.api.nvim_create_autocmd('BufWritePre', {
+local function dump(o)
+    if type(o) == 'table' then
+        local s = '{ '
+
+        for k, v in pairs(o) do
+            if type(k) ~= 'number' then k = '"' .. k .. '"' end
+
+            s = s .. '[' .. k .. '] = ' .. dump(v) .. ','
+        end
+
+        return s .. '} '
+    else
+        return tostring(o)
+    end
+end
+
+local function has_value(tab, val)
+    for _, value in ipairs(tab) do
+        if value == val then
+            return true
+        end
+    end
+
+    return false
+end
+
+local function iter(str)
+    local magicSigns = { '(', ')', '[', ']' };
+    local res = "";
+
+    for c in str:gmatch(".") do
+        if has_value(magicSigns, c) then
+            res = res .. "\\" .. c
+        else
+            res = res .. c
+        end
+    end
+
+    return res
+end
+
+vim.api.nvim_create_autocmd('BufWritePost', {
     pattern = { "*.html", "*.mjs", "*.js", "*.json", "*.jsx", "*.tsx" },
-    command = "silent %!prettierd --find-config-path %",
-    -- command = "silent %!prettierd --single-quote --trailing-comma %",
+    callback = function(c)
+        vim.api.nvim_command(":silent %!prettierd --find-config-path " .. iter(c.file))
+    end,
+    -- command = "silent %!prettierd --find-config-path %",
 })
