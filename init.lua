96a06c4699da7811bf6758a2c7e98b2bb35f57c1
artur <artur200212@interia.pl>
2025-04-28 17:58:21 +0200

    fix: fixed broken which-key

--- init.lua
+++ init.lua
@@ -502,22 +502,22 @@ local on_attach = function(_, bufnr)
 end
 
 -- document existing key chains
-require('which-key').register {
-  ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-  ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-  ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
-  ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
-  ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-  ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-  ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-  ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+require('which-key').add {
+  { '<leader>c', desc = '[C]ode',      hidden = true },
+  { '<leader>d', desc = '[D]ocument',  hidden = true },
+  { '<leader>g', desc = '[G]it',       hidden = true },
+  { '<leader>h', desc = 'Git [H]unk',  hidden = true },
+  { '<leader>r', desc = '[R]ename',    hidden = true },
+  { '<leader>s', desc = '[S]earch',    hidden = true },
+  { '<leader>t', desc = '[T]oggle',    hidden = true },
+  { '<leader>w', desc = '[W]orkspace', hidden = true },
 }
 -- register which-key VISUAL mode
 -- required for visual <leader>hs (hunk stage) to work
-require('which-key').register({
-  ['<leader>'] = { name = 'VISUAL <leader>' },
-  ['<leader>h'] = { 'Git [H]unk' },
-}, { mode = 'v' })
+require('which-key').add {
+  { "<leader>",  group = "VISUAL <leader>", mode = "v" },
+  { "<leader>h", desc = "Git [H]unk",       mode = "v" },
+}
 
 -- mason-lspconfig requires that these setup functions are called in this order
 -- before setting up the servers.
@@ -537,7 +537,6 @@ local servers = {
   -- gopls = {},
   -- pyright = {},
   -- rust_analyzer = {},
-  tsserver = {},
   eslint = {},
   -- intelephense = { filetypes = { "**/php/**", "**/phtml*/" } },
   -- phpactor = { opts = {} },
--- lua/artur/init.lua
+++ lua/artur/init.lua
@@ -1,3 +1,5 @@
 require('artur.remap')
 require('artur.set')
 require('artur.commands')
+
+-- sudo apt-get install xclip ripgrep
