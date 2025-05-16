3b0a0f2495c327123f458e4f6c5ffd93fb22204e
4nxi0uss <artur200212@interia.pl>
2025-05-16 13:45:31 +0200

    feat: update dependencies

--- lua/artur/commands.lua
+++ lua/artur/commands.lua
@@ -1,6 +1,6 @@
-vim.api.nvim_create_autocmd('BufWritePre', {
-    pattern = { "*.html", "*.json" },
-    -- pattern = { "*.html", "*.mjs", "*.js", "*.json", "*.jsx", "*.tsx" },
-    command = "silent %!prettierd --find-config-path %",
-    -- command = "silent %!prettierd --single-quote --trailing-comma %",
-})
+-- vim.api.nvim_create_autocmd('BufWritePre', {
+--     pattern = { "*.html", "*.json" },
+--     -- pattern = { "*.html", "*.mjs", "*.js", "*.json", "*.jsx", "*.tsx" },
+--     command = "silent %!prettierd --find-config-path %",
+--     -- command = "silent %!prettierd --single-quote --trailing-comma %",
+-- })
